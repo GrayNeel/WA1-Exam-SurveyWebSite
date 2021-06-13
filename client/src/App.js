00141d098c66042bf55189c75d8b552d99d86b93
Marco Smorti s281554 <s281554@studenti.polito.it>
2021-06-13 11:18:12 +0200

    feat: session management user side

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import API from './API.js';
 
 function App() {
+  /*************************************** USER SESSION MANAGEMENT ******************************************************/
+
+  /* User info */
+  const [loggedIn, setLoggedIn] = useState(false); // at the beginning, no user is logged in
+  const [message, setMessage] = useState('');      // so no message is shown
+
+  /** Ask server if user is logged in everytime the page is mounted. This information is stored in the cookie of the session */
+  useEffect(() => {
+    API.getUserInfo().then(user => {
+      setLoggedIn(true);
+      setMessage({ msg: `Welcome, ${user.name}!`, type: 'success' });  // Set it again here because otherwise when F5 the message created from LogIn disappears
+    }).catch(error => {
+      setLoggedIn(false);  // No logged user
+    });
+  }, []); // only at mount time
+
+  const doLogIn = async (credentials) => {
+    try {
+      const user = await API.logIn(credentials);
+      setLoggedIn(true);
+      setMessage({ msg: `Welcome, ${user}!`, type: 'success' });
+    } catch (err) {
+      setMessage({ msg: err, type: 'danger' });
+      throw "Incorrect username and/or password";
+    }
+  }
+
+  const doLogOut = async () => {
+    await API.logOut();
+    setLoggedIn(false);
+    // clean up everything
+    //setTasks([]);
+    //setLastId(0);
+  }
+
+  /**********************************************************************************************************************/
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {loggedIn ? message : "Not logged in"}
         </p>
         <a
           className="App-link"
